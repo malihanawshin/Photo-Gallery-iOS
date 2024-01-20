5caaab2631679c090e843e8cafe46dae12ff1808
Maliha Nawshin Rahman <malihanawshin@gmail.com>
2024-01-20 20:05:28 +0600

    Resized photos in gallery  locally

--- GalleryApp/ImageCell.swift
+++ GalleryApp/ImageCell.swift
@@ -9,23 +9,42 @@ import UIKit
 
 class ImageCell : UITableViewCell{
     
-    @IBOutlet weak var ImageView: UIImageView!
+    //@IBOutlet weak var ImageView: UIImageView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-        
+    var mainImageView : UIImageView = {
+        var imageView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    var imageViewHeight = NSLayoutConstraint()
+    var imageRatioWidth = CGFloat()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    
+        self.addSubview(mainImageView)
+        mainImageView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        mainImageView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        mainImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        mainImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder adecoder: NSCoder) {
+        super.init(coder: adecoder)
+        //fatalError("init(coder:) has not been implemented")
     }
     
     
-    func setImageFromUrl(ImageURL :String) {
-       URLSession.shared.dataTask( with: NSURL(string:ImageURL)! as URL, completionHandler: {
-          (data, response, error) -> Void in
-          DispatchQueue.main.async {
-             if let data = data {
-                 self.ImageView.image = UIImage(data: data)
-             }
-          }
-       }).resume()
-    }
+//    func setImageFromUrl(ImageURL :String) {
+//       URLSession.shared.dataTask( with: NSURL(string:ImageURL)! as URL, completionHandler: {
+//          (data, response, error) -> Void in
+//          DispatchQueue.main.async {
+//             if let data = data {
+//                 self.ImageView.image = UIImage(data: data)
+//             }
+//          }
+//       }).resume()
+//    }
 }
--- GalleryApp/ViewController.swift
+++ GalleryApp/ViewController.swift
@@ -9,57 +9,60 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    var images = [UIImage(named: "IMG_4080")!,UIImage(named: "IMG_4081")!,UIImage(named: "IMG_4085")!,UIImage(named: "IMG_4082")!,UIImage(named: "IMG_4097")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //print("Here")
-        // Do any additional setup after loading the view.
-        //tableView.delegate = self
-        tableView.dataSource = self
-        tableView.rowHeight = 160
-        tableView.register(UINib(nibName: "ImageCell", bundle: nil), forCellReuseIdentifier: "ImageCell")
-    }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        1
+        tableView.register(ImageCell.self, forCellReuseIdentifier: "ImageCell")
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return images.count
     }
 
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let image = images[indexPath.row]
+        let imageRatio = CGFloat(image.size.width / image.size.height)
+        
+        return tableView.frame.width / imageRatio
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ImageCell", for: indexPath) as! ImageCell
+        cell.mainImageView.image = images[indexPath.row]
         
-        cell.ImageView.imageFromURL(urlString: "https://source.unsplash.com/random/200x200?sig=2", PlaceHolderImage: UIImage(named: "IMG_4080")!)
+        //imageFromURL(urlString: "https://source.unsplash.com/random/200x200?sig=2", PlaceHolderImage: UIImage(named: "IMG_4080")!)
         
         return cell
     }
     
-
 }
 
-extension UIImageView {
-    
-    public func imageFromURL(urlString: String, PlaceHolderImage: UIImage) {
-        
-        URLSession.shared.dataTask(with: NSURL(string: urlString)! as URL, completionHandler: { (data, response, error) -> Void in
-            
-            if error != nil {
-                print(error ?? "No Error")
-                return
-            }
-            DispatchQueue.main.async(execute: { () -> Void in
-                let image = UIImage(data: data!)
-                self.image = image
-            })
-            
-        }).resume()
-        
-        if self.image == nil{
-            self.image = PlaceHolderImage
-        }
-    }
-    
-    
-}
+// not needed
+
+//extension UIImageView {
+//    
+//    public func imageFromURL(urlString: String, PlaceHolderImage: UIImage) {
+//        
+//        URLSession.shared.dataTask(with: NSURL(string: urlString)! as URL, completionHandler: { (data, response, error) -> Void in
+//            
+//            if error != nil {
+//                print(error ?? "No Error")
+//                return
+//            }
+//            DispatchQueue.main.async(execute: { () -> Void in
+//                let image = UIImage(data: data!)
+//                self.image = image
+//            })
+//            
+//        }).resume()
+//        
+//        if self.image == nil{
+//            self.image = PlaceHolderImage
+//        }
+//    }
+//    
+//    
+//}
